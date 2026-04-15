80313f5116892add9e533f3e30ff81f34e1191a7
Arthur <ach35@njit.edu>
2026-04-15 14:13:56 -0400

    added lab 4

---
+++ Lab 4/cnt1/Contact.java
@@ -0,0 +1,52 @@
+class Contact {
+    //TODO: Create fields
+    private String name;
+    private String email;
+    private String phoneNumber;
+    
+    //Create constructor
+    public Contact(String name, String email, String phoneNumber)
+    {
+        this.name = name;
+        this.email = email
+        this.phoneNumber = phoneNumber;
+    }
+    
+   
+    //Add getters and setters
+    
+    //getters
+    public String getName()
+    {
+        return name;
+    }
+    public String getEmail() 
+    {
+        return email;
+    }
+
+    public String getPhoneNumber() 
+    {
+        return phoneNumber;
+    }
+    
+    // setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+    
+    public String toString() 
+    {
+        //TODO: Return formatted string.
+        return  "Name: " + name + " | Phone: " + phoneNumber;
+    }
+}
